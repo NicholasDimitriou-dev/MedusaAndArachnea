5e3b32d35e98b5399548a2a9fbcb8844c6b6bf70
Chris Rensel-Smith <32113009+0Starus@users.noreply.github.com>
2026-04-10 10:56:59 -0700

    Movement working (Finally, controllers don't work)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class Player : MonoBehaviour{
+using UnityEngine.InputSystem.Controls;
 
+public class Player : MonoBehaviour{
+    public enum Character
+    {
+        Arachnea,
+        Medusa,
+    } 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float walkSpeed = 5f;
     public float runSpeed = 10f;
@@ -11,12 +17,58 @@ public class Player : MonoBehaviour{
     Vector2 _velocity;
     Quaternion facingRight;
     Quaternion facingLeft;
-    [SerializeField] private CharacterController medusaController;
-    [SerializeField] private CharacterController arachneController;
-    [SerializeField] private Transform medusaTransform;
-    [SerializeField] private Transform arachneTransform;
+    private CharacterController controller;
+    private KeyControl up;
+    private KeyControl down;
+    private KeyControl left;
+    private KeyControl right;
+    private KeyControl dash;
+    private KeyControl interact;
+    private ButtonControl controllerUp;
+    private ButtonControl controllerDown;
+    private ButtonControl controllerLeft;
+    private ButtonControl controllerRight;
+    private ButtonControl controllerDash;
+    private ButtonControl controllerInteract;
+
+    public Character character;
+    void Awake()
+    {
+        if (character == Character.Arachnea)
+        {
+            up = Keyboard.current.upArrowKey;
+            down = Keyboard.current.downArrowKey;
+            left = Keyboard.current.leftArrowKey;
+            right = Keyboard.current.rightArrowKey;
+            dash = Keyboard.current.rightShiftKey;
+            interact = Keyboard.current.enterKey;
+            // controllerUp = Gamepad.current.buttonNorth;
+            // controllerDown = Gamepad.current.buttonSouth;
+            // controllerLeft = Gamepad.current.buttonWest;
+            // controllerRight = Gamepad.current.buttonEast;
+            // controllerDash = Gamepad.current.rightShoulder;
+            // controllerInteract = Gamepad.current.rightTrigger;
+        }
+        else
+        {
+            up = Keyboard.current.wKey;
+            down = Keyboard.current.sKey;
+            left = Keyboard.current.aKey;
+            right = Keyboard.current.dKey;
+            dash = Keyboard.current.leftShiftKey;
+            interact = Keyboard.current.eKey;
+            // controllerUp = Gamepad.current.dpad.up;
+            // controllerDown = Gamepad.current.dpad.down;
+            // controllerLeft = Gamepad.current.dpad.left;
+            // controllerRight = Gamepad.current.dpad.right;
+            // controllerDash = Gamepad.current.leftShoulder;
+            // controllerInteract = Gamepad.current.leftTrigger;
+        }
+    }
     void Start()
     {
+        
+        controller = GetComponent<CharacterController>();
         facingRight = Quaternion.Euler(0f,90f,0f);
         facingLeft = Quaternion.Euler(0f,270f,0f);
     }
@@ -25,14 +77,18 @@ public class Player : MonoBehaviour{
     void Update()
     {
         float direction = 0f;
-        if(Keyboard.current.dKey.isPressed) direction += 1f;
-        if(Keyboard.current.aKey.isPressed) direction -= 1f;
-        bool jumpPressedThisFrame = Keyboard.current.wKey.wasPressedThisFrame;
-        bool jumpHeld = Keyboard.current.wKey.isPressed;
+        // if(right.isPressed||controllerRight.isPressed) direction += 1f;
+        // if(left.isPressed||controllerLeft.isPressed) direction -= 1f;
+        // bool jumpPressedThisFrame = up.wasPressedThisFrame||controllerUp.wasPressedThisFrame;
+        // bool jumpHeld = up.isPressed||controllerUp.isPressed;
+        if(right.isPressed) direction += 1f;
+        if(left.isPressed) direction -= 1f;
+        bool jumpPressedThisFrame = up.wasPressedThisFrame;
+        bool jumpHeld = up.isPressed;
 
         float gravityMod = 1f;
 
-        if (arachneController.isGrounded)
+        if (controller.isGrounded)
         {
             if (direction!= 0f)
             {
@@ -43,7 +99,7 @@ public class Player : MonoBehaviour{
                 _velocity.x += direction*groundAcceleration * Time.deltaTime;
                 _velocity.x = Mathf.Clamp(_velocity.x,-walkSpeed,walkSpeed);
 
-                arachneTransform.rotation = (direction >0f) ? facingRight : facingLeft;
+                transform.rotation = (direction >0f) ? facingRight : facingLeft;
             }
             else
             {
@@ -62,7 +118,7 @@ public class Player : MonoBehaviour{
             }
         }
 
-        if (!arachneController.isGrounded)
+        if (!controller.isGrounded)
         {
             float gravity = 2f*apexHeight/(apexTime*apexTime);
             _velocity.y -= gravity*gravityMod*Time.deltaTime;
@@ -71,8 +127,8 @@ public class Player : MonoBehaviour{
         float deltaX = _velocity.x*Time.deltaTime;
         float deltaY = _velocity.y*Time.deltaTime;
         Vector3 deltaPosition = new Vector3(deltaX,deltaY,0f);
-        arachneTransform.position += deltaPosition;
-        
+        transform.position += deltaPosition;
+        controller.Move(deltaPosition);
     }
 
 }
