ed8efc685c85b6cd26023ac629f0b8b4065a6307
Kobesowner712 <dougiemcd@cox.net>
2026-04-12 19:57:48 -0700

    Enemy Movement (Using NavMesh) Implemented

--- Assets/Script_Nicholas/Enemy.cs
+++ Assets/Script_Nicholas/Enemy.cs
@@ -1,12 +1,38 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private NavMeshAgent agent;
+    [SerializeField] private EnemyPath enemyPath;
+    private Transform destination;
+    
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        destination = enemyPath.GetPositionTwo();
     }
+
+
+    private void Update()
+    {
+        agent.SetDestination(destination.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Destination One"))
+        {
+            destination = enemyPath.GetPositionTwo();
+        }
+        else if (other.CompareTag("Destination Two"))
+        {
+            destination = enemyPath.GetPositionOne();
+        }
+    }
+
 
     public void TurnToStone(Transform stonePrefab)
     {
