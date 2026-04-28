842bc6301b0a0a8a4fc22d0b27188d21c7a9e07e
Shiro219 <rumendez@csumb.edu>
2026-04-28 12:12:04 -0700

    Timer and coin collection

--- Assets/Rull's Work/Rull_Scripts/GameManager.cs
+++ Assets/Rull's Work/Rull_Scripts/GameManager.cs
@@ -52,14 +52,33 @@ public class GameManager : MonoBehaviour
     {
         gameEnded = true;
         Debug.Log("You win!");
+
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextIndex = currentIndex + 1;
+
+        if (nextIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextIndex);
+        }
+        else
+        {
+            Debug.Log("Level completed!");
+            SceneManager.LoadScene(1);
+
+        }
+    }
+    /*private void WinGame()
+    {
+        gameEnded = true;
+        Debug.Log("You win!");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    */
 
     private void LoseGame()
     {
         gameEnded = true;
         Debug.Log("Time's up!");
-        // Reload level
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
